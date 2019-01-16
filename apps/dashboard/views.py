feb813132986648270bde2fb058459c97753b2f5
Dy0mar <kurtudins@gmail.com>
2019-01-16 18:46:25 +0200

    add CreateTaskView

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.utils import timezone
+from django.views.generic import CreateView
 from django.views.generic import ListView
 
-from dashboard.models import Task
+from dashboard.forms import CreateTaskForm
+from dashboard.models import Task, TaskLog
 
 
 class TaskListView(LoginRequiredMixin, ListView):
@@ -13,10 +16,39 @@ class TaskListView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
-        context['new_tasks'] = Task.objects.filter(status=Task.TODO)
-        context['in_progress_tasks'] = Task.objects.filter(
-            status=Task.IN_PROGRESS
-        )
-        context['done_tasks'] = Task.objects.filter(status=Task.TODO)
+        new_tasks = in_progress_tasks = done_tasks = 0
+        for task in self.model.objects.all():
+            if task.status == self.model.TODO:
+                new_tasks += 1
+            if task.status == self.model.IN_PROGRESS:
+                in_progress_tasks += 1
+            if task.status == self.model.DONE:
+                done_tasks += 1
 
+        context['new_tasks'] = new_tasks
+        context['in_progress_tasks'] = in_progress_tasks
+        context['done_tasks'] = done_tasks
+
+        return context
+
+
+class CreateTaskView(LoginRequiredMixin, CreateView):
+    template_name = 'dashboard/task_create.html'
+    model = Task
+    form_class = CreateTaskForm
+
+    def form_valid(self, form):
+        task = form.save(commit=False)
+        task.author = self.request.user
+        task.save()
+        TaskLog.objects.create(
+            action=TaskLog.CREATE_TASK,
+            task=task,
+            data=None,
+            user=self.request.user,
+        )
+        return redirect('home')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CreateTaskView, self).get_context_data(**kwargs)
         return context
